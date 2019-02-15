18761838206182f746779e37d5e3494be72fca59
Dario <salina.dario@gmail.com>
2019-02-15 17:15:11 +0100

    first try to make socket working

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,26 @@
 import 'reflect-metadata'
 import {createKoaServer} from 'routing-controllers'
 import Controller from './controller'
-// import * as Koa from 'koa'
-// import * as IO from 'socket.io'
+import * as Koa from 'koa'
+import * as IO from 'socket.io'
+import {Server} from 'http'
 
+const app = new Koa()
+const server = new Server(app.callback())
+export const io = IO(server)
 const port = process.env.PORT || 4000
 
-const app = createKoaServer({
+createKoaServer({
     cors: true,
     controllers: [Controller]
 })
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`))
 
+io.on('connection', function(socket) {
+
+  console.log(`User  just connected`)
+
+  socket.on('disconnect', () => {
+    console.log(`User  just disconnected`)}
+)});
