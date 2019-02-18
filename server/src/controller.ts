870b307f9d2748542f133fef48ba6d8682b48712
Dario <salina.dario@gmail.com>
2019-02-18 10:19:34 +0100

    server working, socket still not

--- server/src/controller.ts
+++ server/src/controller.ts
@@ -2,13 +2,13 @@ import {Controller, Get} from 'routing-controllers'
 // import { isMainThread } from 'worker_threads';
 
 @Controller()
-export default class {@Get('/test')
+export default class {
+    
+@Get('/test')
 main(){
+    console.log('test')
     return {
         hello: 'test'
     }
+  }
 }
-
-
-}
-    
