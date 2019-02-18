eb2391416b3ef90c36051181c9bdea37326be7a2
Joey Grolleman <joeygrolleman@gmail.com>
2019-02-18 10:50:09 +0100

    set up react app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PlayerContainer from './components/PlayerContainer'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PlayerContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
