26f55352b7605c80b5a3401497df53d362c33e36
Joey Grolleman <joeygrolleman@gmail.com>
2019-02-20 10:43:07 +0100

    players info with id send back to clients

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,12 @@
 import openSocket from 'socket.io-client';
 const socket = openSocket('http://localhost:5000/');
 
+socket.on('update-players', data => {
+    console.log(data)
+}
+)
+
+
 
 export default function newPlayer() {
         socket.emit('new-player', {
